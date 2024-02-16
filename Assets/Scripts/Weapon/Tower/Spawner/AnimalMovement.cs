d76e5c51e20ebdb58c6f7a7b23af58623a8c572d
Mitchell Oliarny <mitchelloliarny@gmail.com>
2024-02-15 19:36:50 -0700

    VFX

---
+++ Assets/Scripts/Weapon/Tower/Spawner/AnimalMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalMovement : MonoBehaviour
+{
+    private GameObject spawner;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
